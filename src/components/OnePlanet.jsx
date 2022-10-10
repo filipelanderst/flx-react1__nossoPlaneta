f820d71af31b5672bb2720f7e4d82418c3e7b6d6
Filipe Lander <flxcode7@onmail.com>
2022-10-10 15:41:27 -0300

    video-test

--- src/components/OnePlanet.jsx
+++ src/components/OnePlanet.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
+const youtube = (
+  <iframe
+    width='100%'
+    height='100%'
+    src='https://www.youtube.com/embed/4iL3M4Iv2R4'
+    title='YouTube video player'
+    frameborder='0'
+    allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+    allowfullscreen
+  ></iframe>
+);
 
 const OnePlanet = () => {
   return (
     <div id='one-planet' className='grid sm:grid-cols-2'>
       <div className='relative'>
-        <a href='our-planet.mp4'>
+        <a href='https://www.dropbox.com/home?preview=our-planet-teaser.mp4'>
           <img
             src='https://raw.githubusercontent.com/flx-lander7/flx-react1__nossoPlaneta/main/public/img/one-planet.png'
             alt=''
           />
         </a>
         <div className='play-icon'>
-          <a href='our-planet-teaser.mp4'>
+          <a href='https://www.dropbox.com/home?preview=our-planet-teaser.mp4'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               fill='none'
